b5f0b4f913dbb227fad7a6fff14967d3173df34d
Bruno Silva <brunohenriquegtsilva@gmail.com>
2025-11-27 16:37:57 -0500

    Added UserCourses Entety class

--- src/main/java/com/brunosilva/terpbuddy/Entities/UserCourses.java
+++ src/main/java/com/brunosilva/terpbuddy/Entities/UserCourses.java
@@ -1,4 +1,67 @@
 package com.brunosilva.terpbuddy.Entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user_classes")
 public class UserCourses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many UserCourses entries can reference the same user
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users user;
+
+    // Many UserCourses entries can reference the same course
+    @ManyToOne
+    @JoinColumn(name = "class_id", nullable = false)
+    private UMDCourses course;
+
+    // No-args constructor required by JPA
+    public UserCourses() {}
+
+    // Constructor for required fields
+    public UserCourses(Users user, UMDCourses course) {
+        this.user = user;
+        this.course = course;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public UMDCourses getCourse() {
+        return course;
+    }
+
+    public void setCourse(UMDCourses course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "UserCourses{" +
+                "id=" + id +
+                ", user=" + user.getId() +
+                ", course=" + course.getId() +
+                '}';
+    }
 }
