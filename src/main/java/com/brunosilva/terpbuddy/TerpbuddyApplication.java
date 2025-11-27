df6ee9c78dc2dfb292b69c067f7fc504bad88347
Bruno Silva <brunohenriquegtsilva@gmail.com>
2025-11-27 15:24:30 -0500

    Initial commit

---
+++ src/main/java/com/brunosilva/terpbuddy/TerpbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.brunosilva.terpbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerpbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerpbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunosilva/terpbuddy/TerpbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosilva.terpbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerpbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
